e4aa377c9f6bb39685a3d725e94732869a8c5f7a
Pravin Patel <patelpra337@gmail.com>
2019-12-05 21:09:32 -0600

    Create storyboard, views and outlets

---
+++ Stars (iOSPT4)/StarsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  StarsViewController.swift
+//  Stars (iOSPT4)
+//
+//  Created by patelpra on 12/5/19.
+//  Copyright © 2019 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+class StarsViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var distanceTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func printStars(_ sender: UIButton) {
+        
+        
+    }
+    
+
+    @IBAction func createStar(_ sender: UIButton) {
+        
+        
+    }
+    
+}
+
