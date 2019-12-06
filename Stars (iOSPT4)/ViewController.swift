bb5bdb58c0e75c2c1cf17818a07e7a6e036b53a1
Pravin Patel <patelpra337@gmail.com>
2019-12-05 20:39:38 -0600

    Initialize new project Stars

---
+++ Stars (iOSPT4)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars (iOSPT4)
+//
+//  Created by patelpra on 12/5/19.
+//  Copyright © 2019 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
